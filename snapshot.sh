296baaa763b7ed46d8190beb4e391a0bda19b791
Motko <77466238+Motko222@users.noreply.github.com>
2024-05-17 10:48:41 +0200

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -21,13 +21,15 @@ initiad comet unsafe-reset-all --home $HOME/.initia --keep-addr-book
 sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.initia/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.initia/config/app.toml
 sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"0\"/" $HOME/.initia/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.initia/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" $HOME/.initia/config/app.toml
 sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.initia/config/config.toml
 
 # Disable state sync
 sed -i -e "s/^enable *=.*/enable = false/" $HOME/.initia/config/config.toml
 
-curl https://snapshots.kjnodes.com/initia-testnet/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.initia
+echo "Snapshot height? (https://polkachu.com/testnets/initia/snapshots) : height
+
+curl https://snapshots.polkachu.com/testnet-snapshots/initia/initia_$height.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.initia
 
 # Restore the priv_validator_state
 mv $HOME/.initia/priv_validator_state.json.backup $HOME/.initia/data/priv_validator_state.json
