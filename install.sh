8ab3c01f232a7129447fd39c1db48b2f24d92e4a
Motko <77466238+Motko222@users.noreply.github.com>
2024-05-17 06:12:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 config=~/scripts/$folder/cfg
 
+# Update and install dependencies
+sudo apt update && sudo apt install -y curl git jq build-essential gcc unzip wget lz4
+
+ufw disable
+# Install Golang
+wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
 #install binary
 cd ~
 git clone https://github.com/initia-labs/initia.git
