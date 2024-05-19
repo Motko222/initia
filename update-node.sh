ce512d101193c0105f945c09ca121a044daac9aa
Motko <77466238+Motko222@users.noreply.github.com>
2024-05-19 22:28:07 +0200

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$folder/cfg
+
+sudo systemctl stop $BINARY.service
+
 cd ~
 git clone https://github.com/initia-labs/initia.git
 cd initia
